1373fe3dbf95dbb986171f0cac80137363e02ca6
Joey Tamondong <joeytamondong2001@gmail.com>
2025-06-12 18:03:37 +0800

    return json to bakend if its running

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(cookie());
 
 // server
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+    res.json((`Server is running on port ${process.env.PORT}`));
 })
 
 // database connection
