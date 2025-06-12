ec5fbe6c7c524ad7ea6cc30e03e1f0a88cc9e6e2
Joey Tamondong <joeytamondong2001@gmail.com>
2025-06-12 18:26:58 +0800

    Fix everything

--- backend/index.js
+++ backend/index.js
@@ -4,20 +4,26 @@ import 'dotenv/config'
 import cookie from 'cookie-parser'
 import connectDB from './connection/connect.js';
 import route from './routes/route.js';
-
+import path from 'path'
 //Instance of express
 const app = express();
+const __dirname = path.resolve()
+
+if(process.env.NODE_ENV !=="production"){
 
 app.use(cors({
     origin: process.env.ORIGIN_URL,
     credentials: true
 }));
+
+}
+
 app.use(express.json());
 app.use(cookie());
 
 // server
 app.listen(process.env.PORT, () => {
-    res.json((`Server is running on port ${process.env.PORT}`));
+    console.log(`Server is running on port ${process.env.PORT}`);
 })
 
 // database connection
@@ -25,3 +31,10 @@ connectDB();
 
 // Routes
 app.use('/api',route)
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static.join(__dirname,"../frontend/dist"))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
